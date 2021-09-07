1e5808783b0813835c7fa4b34497d197656ae9a2
William <williamchenjian@gmail.com>
2021-09-06 23:10:19 -0400

    change button border

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,13 @@ import styled from 'styled-components'
 const Button = styled.button`
     background: transparent;
     border-radius: 3px;
-    border: 2px solid #BAABBD;
+    border: 5px solid #BAABBD;
     color: #BAABBD;
     margin: 1em;
     padding: 0.25em 1em;
     font-family: 'Poppins', sans-serif;
     font-size: 25px;
-    font-weight: 400;
+    font-weight: 500;
 
     &:hover {
         filter: brightness(75%);
